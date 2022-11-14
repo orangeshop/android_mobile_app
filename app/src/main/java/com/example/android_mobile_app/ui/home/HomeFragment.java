20ddab1d369f5367b051aa665502d9b689ded5e7
choeyeongho <dudgh102416@gmail.com>
2022-11-15 00:17:19 +0900

    up

--- app/src/main/java/com/example/android_mobile_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/android_mobile_app/ui/home/HomeFragment.java
@@ -15,6 +15,9 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
